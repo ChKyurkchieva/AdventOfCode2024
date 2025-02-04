41c1effeee0d2f7d77c0b5fb35051cfcccb15398
Christina Kyurkchieva <48100614+ChKyurkchieva@users.noreply.github.com>
2025-02-04 17:24:27 +0200

    Trying to solve first task of day 5;

---
+++ AdventOfCode2024/Day 5/FifthDay.cs
@@ -0,0 +1,117 @@
+﻿using System.Data;
+using System.Data.Common;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024.Day_5;
+
+internal class FifthDay
+{
+	private List<List<int>> _graph;
+	public FifthDay(string[] text) => _graph = CreateGraph(text);
+	public List<List<int>> CreateGraph(string[] text)
+	{
+		List<List<int>> graph = new List<List<int>>();
+		for (int i = 1; i < 100; i++)
+		{
+			graph.Add(new List<int>());
+		}
+		foreach (string line in text)
+		{
+			if (line == string.Empty || !line.Contains('|'))
+				return graph;
+
+			var match = Regex.Match(line, @"([0-9]+)\|([0-9]+)");
+			if (match.Success)
+			{
+				var a = match.Groups[1];
+				var b = match.Groups[2];
+				int.TryParse(a.ToString(), out int first);
+				int.TryParse(b.ToString(), out int second);
+				graph[first].Add(second);
+			}
+		}
+		return graph;
+	}
+	public static void DisplayGraph(List<List<int>> graph)
+	{
+		for (int i = 0; i < graph.Count; i++)
+		{
+			Console.Write($"{i}: "); // Print the vertex
+			foreach (int j in graph[i])
+			{
+				Console.Write($"{j} "); // Print its adjacent
+			}
+			Console.WriteLine();
+		}
+	}
+	public  bool IsSubsequence(List<int> numbers, List<int> sequence)
+	{
+		if(numbers.Count == 0) return false;
+		List<int> indeces = new List<int>();
+		foreach (int n in sequence)
+			indeces.Add(numbers.IndexOf(n));
+		var ordered = indeces.Order();
+		for (int i = 0; i < ordered.Count(); i++)
+			if (ordered.ElementAt(i) != indeces[i]) return false;
+
+		return true;
+	}
+	private void DepthFirstSearch(int start, List<int> path)
+	{
+		Boolean[] visited = new Boolean[100];
+
+		Stack<int> stack = new Stack<int>();
+
+		stack.Push(start);
+
+		while (stack.Count > 0)
+		{
+			start = stack.Peek();
+			stack.Pop();
+
+			if (visited[start] == false)
+			{
+				path.Add(start);
+				visited[start] = true;
+			}
+
+			foreach (int v in _graph[start])
+			{
+				if (!visited[v] && _graph[v].Count > 0)
+					stack.Push(v);
+			}
+
+		}
+	}
+
+	public int SubsequencesMultiplication(string[] text)
+	{
+		int result = 0;
+		foreach (string line in text)
+		{
+			var match = Regex.Match(line, @"((([0-9]+),)+([0-9]+))", RegexOptions.Multiline);
+			if (match.Success)
+			{
+				string numbers = match.Value;
+				List<int> sequence = new List<int>();
+				string[] strings = numbers.Split(',');
+				foreach (var s in strings)
+				{
+					int.TryParse(s, out int number);
+					sequence.Add(number);
+				}
+				for (int i = 0; i < sequence.Count - 1; i++)
+				{
+					List<int> candidate = new List<int>();
+					DepthFirstSearch(sequence[i], candidate);
+					if (!candidate.Contains(sequence[i + 1]))
+						break;
+					if (i + 1 == sequence.Count - 1)
+						result += sequence[sequence.Count / 2];
+				}
+			}
+		}
+		return result;
+	}
+}
--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -1,68 +1,83 @@
-﻿//day1 
-
-using AdventOfCode2024.Day_1;
+﻿using AdventOfCode2024.Day_1;
 using AdventOfCode2024.Day_2;
 using static System.Net.Mime.MediaTypeNames;
 using System.Text.RegularExpressions;
 using AdventOfCode2024.Day_3;
 using AdventOfCode2024.Day_4;
+using AdventOfCode2024.Day_5;
 
-//DAY 1
-//List<int> first = new List<int>();
-//List<int> second = new List<int>();
-//string firstfilePath = @"..\..\..\Day 1\Numbers.txt";
-//string[] lines = File.ReadAllLines(firstfilePath);
-
-//foreach (string l in lines)
+////DAY 1
 //{
-//	string[] parts = l.Split("   ");
-//	if (int.TryParse(parts[0], out int number1) && int.TryParse(parts[1], out int number2))
+//	List<int> first = new List<int>();
+//	List<int> second = new List<int>();
+//	string firstfilePath = @"..\..\..\Day 1\Numbers.txt";
+//	string[] lines = File.ReadAllLines(firstfilePath);
+
+//	foreach (string l in lines)
 //	{
-//		first.Add(number1);
-//		second.Add(number2);
+//		string[] parts = l.Split("   ");
+//		if (int.TryParse(parts[0], out int number1) && int.TryParse(parts[1], out int number2))
+//		{
+//			first.Add(number1);
+//			second.Add(number2);
+//		}
 //	}
+//	FirstDayTask1 firstDay = new FirstDayTask1();
+//	Console.WriteLine(firstDay.Distance(first, second));
+//	Console.WriteLine(firstDay.Similarity(first, second));
 //}
-//FirstDayTask1 firstDay = new FirstDayTask1();
-//Console.WriteLine(firstDay.Distance(first, second));
-//Console.WriteLine(firstDay.Similarity(first, second));
 
-//DAY 2
-
-//string secondfilePath = @"..\..\..\Day 2\Numbers.txt";
-//string[] lineNumbers = File.ReadAllLines(secondfilePath);
-//int safeTolerate = 0;
-//int safe = 0;
-//SecondDay secondDay = new SecondDay();
-//int index = 0;
-//foreach(string l in lineNumbers)
+////DAY 2
 //{
-//	string[] parts = l.Split(" ");
-//	List<int> line = new List<int>();
-//	foreach (string i in parts)
+//	string secondfilePath = @"..\..\..\Day 2\Numbers.txt";
+//	string[] lineNumbers = File.ReadAllLines(secondfilePath);
+//	int safeTolerate = 0;
+//	int safe = 0;
+//	SecondDay secondDay = new SecondDay();
+//	int index = 0;
+//	foreach (string l in lineNumbers)
 //	{
-//		if(int.TryParse(i, out int number))
-//			line.Add(number);
+//		string[] parts = l.Split(" ");
+//		List<int> line = new List<int>();
+//		foreach (string i in parts)
+//		{
+//			if (int.TryParse(i, out int number))
+//				line.Add(number);
+//		}
+//		safe += secondDay.SafeList(line);
+//		safeTolerate += secondDay.SafeTolerate(line);
 //	}
-//	safe += secondDay.SafeList(line);
-//	safeTolerate += secondDay.SafeTolerate(line);
+//	Console.WriteLine(safe);
+//	Console.WriteLine(safeTolerate);
 //}
-//Console.WriteLine(safe);
-//Console.WriteLine(safeTolerate);
 
-//DAY 3
-//string filePath = @"..\..\..\Day 3\Text.txt";
-//
-//ThirdDay thirdDay = new ThirdDay();
-//Int128 result = thirdDay.SumOfMultiplications(lines);
-//Int128 result2 = thirdDay.SumOfDoMultiplications(lines);
-//Console.WriteLine(result);
-//Console.WriteLine(result2);
+////DAY 3
+//{
+//	string filePath = @"..\..\..\Day 3\Text.txt";
+//	string[] lines = File.ReadAllLines(filePath);
+//	ThirdDay thirdDay = new ThirdDay();
+//	Int128 result = thirdDay.SumOfMultiplications(lines);
+//	Int128 result2 = thirdDay.SumOfDoMultiplications(lines);
+//	Console.WriteLine(result);
+//	Console.WriteLine(result2);
+//}
 
-//DAY 4
-string filePath = @"..\..\..\Day 4\Text.txt";
-string[] lines = File.ReadAllLines(filePath);
-FourthDay fourthDay = new FourthDay();
-Int128 result = fourthDay.XmasNumber(lines);
-Int128 result1 = fourthDay.MasX(lines);
-Console.WriteLine(result);
-Console.WriteLine(result1);
+////DAY 4
+//{
+//	string filePath = @"..\..\..\Day 4\Text.txt";
+//	string[] lines = File.ReadAllLines(filePath);
+//	FourthDay fourthDay = new FourthDay();
+//	Int128 result = fourthDay.XmasNumber(lines);
+//	Int128 result1 = fourthDay.MasX(lines);
+//	Console.WriteLine(result);
+//	Console.WriteLine(result1);
+//}
+
+//DAY 5
+{
+	string filePath = @"..\..\..\Day 5\Input.txt";
+	string[] lines = File.ReadAllLines(filePath);
+	FifthDay fifthDay = new FifthDay(lines);
+	int result = fifthDay.SubsequencesMultiplication(lines);
+	Console.WriteLine(result);
+}
