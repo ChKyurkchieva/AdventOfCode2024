05b1e8b039e9596f484e567ec959a279f8da4385
Christina Kyurkchieva <48100614+ChKyurkchieva@users.noreply.github.com>
2025-02-26 17:04:58 +0200

    alt-enter refactoring of SeventhDay first task

--- AdventOfCode2024/Day 7/SeventhDay.cs
+++ AdventOfCode2024/Day 7/SeventhDay.cs
@@ -5,17 +5,6 @@ namespace AdventOfCode2024.Day_7;
 
 internal class SeventhDay
 {
-	private static string[] GenerateAllPermutations(int n)
-	{
-		int size = (int)Math.Pow(2, n);
-		string[] result = new string[size];
-		for (int i = 0; i < size; i++)
-		{
-			string binary = Convert.ToString(i, 2).PadLeft(n, '0');
-			result[i] = binary;
-		}
-		return result;
-	}
 	public static UInt128 FindTotalCalibrationResult()
 	{
 		UInt128 result = 0;
@@ -26,30 +15,61 @@ internal class SeventhDay
 			MatchCollection matches = Regex.Matches(line, "([0-9]+){1}");
 			if (matches.Count > 0)
 			{
-				List<UInt128> numbers = new(matches.Count);
-				for (int i = 0; i < matches.Count; i++)
-				{
-					if(UInt128.TryParse(matches.ElementAt(i).ToString(), out UInt128 n))
-						numbers.Add(n);
-				}
-				string[] permutations = GenerateAllPermutations(numbers.Count - 2);
-				foreach (var permutation in permutations)
-				{
-					UInt128 candidate = numbers[1];
-					for (int i = 0; i < permutation.Length; i++)
-					{
-						if (permutation[i] == '0')
-							candidate += numbers[i + 2];
-						if (permutation[i] == '1')
-							candidate *= numbers[i + 2];
-					}
-					if (candidate == numbers[0])
-					{
-						result += (UInt128)numbers[0];
-						break;
-					}
-				}
+				List<UInt128> numbers = ExtractNumbersToList(matches);
+				result = FindCalibrationsNumber(result, numbers);
 			}
+		}
+		return result;
+	}
+
+	private static List<UInt128> ExtractNumbersToList(MatchCollection matches)
+	{
+		List<UInt128> numbers = new(matches.Count);
+		for (int i = 0; i < matches.Count; i++)
+		{
+			if (UInt128.TryParse(matches.ElementAt(i).ToString(), out UInt128 n))
+				numbers.Add(n);
+		}
+
+		return numbers;
+	}
+
+	private static UInt128 FindCalibrationsNumber(UInt128 result, List<UInt128> numbers)
+	{
+		string[] permutations = GenerateAllPermutations(numbers.Count - 2);
+		foreach (var permutation in permutations)
+		{
+			UInt128 candidate = CalibrationNumberCandidate(numbers, permutation);
+			if (candidate == numbers[0])
+			{
+				result += numbers[0];
+				break;
+			}
+		}
+		return result;
+	}
+
+	private static UInt128 CalibrationNumberCandidate(List<UInt128> numbers, string permutation)
+	{
+		UInt128 candidate = numbers[1];
+		for (int i = 0; i < permutation.Length; i++)
+		{
+			if (permutation[i] == '0')
+				candidate += numbers[i + 2];
+			if (permutation[i] == '1')
+				candidate *= numbers[i + 2];
+		}
+		return candidate;
+	}
+
+	private static string[] GenerateAllPermutations(int n)
+	{
+		int size = (int)Math.Pow(2, n);
+		string[] result = new string[size];
+		for (int i = 0; i < size; i++)
+		{
+			string binary = Convert.ToString(i, 2).PadLeft(n, '0');
+			result[i] = binary;
 		}
 		return result;
 	}
